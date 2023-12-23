ba4cb6075d85f224576f1b1d3f4ca535aa578982
cuauhvip07 <cuauvillalba1@gmail.com>
2023-12-22 23:15:43 -0600

    Prueba1
    Cambio de codigo

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1>Hola desde udemy aqui se cambia todo </h1>
+    <h1>Hola desde udemy aqui se cambia todo. </h1>
   )
 }
 
