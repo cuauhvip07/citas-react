af81495c2b3bc78f283094c8315bd9c9c4dae2c8
cuauhvip07 <cuauvillalba1@gmail.com>
2023-12-23 21:16:32 -0600

    Finalizacion lista pacientes
    Finalizacion del archivo lista de pacientes en la parte del diseño

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,19 @@
+
+
+const Paciente = () => {
+    return (
+        <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+            <p className="font-bold mb-3 text-gray-700 uppercase">Nombre:{' '}<span className="font-normal normal-case">Hook</span></p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Apellido:{' '}<span className="font-normal normal-case">Gomez</span></p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Email:{' '}<span className="font-normal normal-case">correo@correo.com</span></p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Fecha:{' '}<span className="font-normal normal-case">12/02/2023</span></p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Sintomas:{' '}<span className="font-normal normal-case">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur amet pariatur omnis neque veritatis, reiciendis aut facere assumenda sit, consectetur necessitatibus quaerat ad provident, ipsum asperiores ea ab delectus sint!</span></p>
+        </div>
+    )
+}
+
+export default Paciente
