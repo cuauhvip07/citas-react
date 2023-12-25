cfee4759e8f768e42ff73f07f46c62b2d2d39873
cuauhvip07 <cuauvillalba1@gmail.com>
2023-12-25 16:41:08 -0600

    Prop dinamico pacientes
    Se crean los props dinamicos para poder ir guardando los datos de los pacientes, quitando el html estatico a dinamico

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,17 +1,25 @@
 import Paciente from "./Paciente";
 
 
-function ListadoPacientes(){
+function ListadoPacientes({pacientes}){
+
 
     return(
         <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
             <h2 className="font-black text-3xl text-center">Listado pacientes</h2>
             <p className="text-xl text-center mt-5 mb-10 ">Administra tus {' '}<span className="text-indigo-600 font-bold">Pacientes y Citas</span></p>
 
-            <Paciente />
-            <Paciente />
-            <Paciente />
-            <Paciente />
+            {
+                pacientes.map( paciente => {
+                    return(
+                        <Paciente
+                            paciente = {paciente}
+                        />
+                    )
+                })
+            }
+
+
         </div>
     )
 }
--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,17 +1,19 @@
 
 
-const Paciente = () => {
+const Paciente = ({paciente}) => {
+
+    const {nombre,apellido,email,fecha,sintomas} = paciente
     return (
         <div className="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
-            <p className="font-bold mb-3 text-gray-700 uppercase">Nombre:{' '}<span className="font-normal normal-case">Hook</span></p>
+            <p className="font-bold mb-3 text-gray-700 uppercase">Nombre:{' '}<span className="font-normal normal-case">{nombre}</span></p>
 
-            <p className="font-bold mb-3 text-gray-700 uppercase">Apellido:{' '}<span className="font-normal normal-case">Gomez</span></p>
+            <p className="font-bold mb-3 text-gray-700 uppercase">Apellido:{' '}<span className="font-normal normal-case">{apellido}</span></p>
 
-            <p className="font-bold mb-3 text-gray-700 uppercase">Email:{' '}<span className="font-normal normal-case">correo@correo.com</span></p>
+            <p className="font-bold mb-3 text-gray-700 uppercase">Email:{' '}<span className="font-normal normal-case">{email}</span></p>
 
-            <p className="font-bold mb-3 text-gray-700 uppercase">Fecha:{' '}<span className="font-normal normal-case">12/02/2023</span></p>
+            <p className="font-bold mb-3 text-gray-700 uppercase">Fecha:{' '}<span className="font-normal normal-case">{fecha}</span></p>
 
-            <p className="font-bold mb-3 text-gray-700 uppercase">Sintomas:{' '}<span className="font-normal normal-case">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur amet pariatur omnis neque veritatis, reiciendis aut facere assumenda sit, consectetur necessitatibus quaerat ad provident, ipsum asperiores ea ab delectus sint!</span></p>
+            <p className="font-bold mb-3 text-gray-700 uppercase">Sintomas:{' '}<span className="font-normal normal-case">{sintomas}</span></p>
         </div>
     )
 }
